f6086142f78ec1ecd30e7243d6d1ece06265a0c3
adrianajd <ajuarezd@megacable.com.mx>
2025-04-15 09:33:00 -0600

    Actividad 1
    Caracteres no soportados por utf-8.
    Adriana Juarez

---
+++ Modulo_1_Javascript_Testing/01_sesion/README.md
@@ -0,0 +1,11 @@
+
+
+Actividad 01
+Adriana Saamay Juarez Dominguez
+
+Fecha: 24/03/2025
+HTML y Javascript
+
+📌 **Características principales:**  
+Funcionamiento basico de pagina web.Identificacion de etiquetas esenciales en el lenguaje de HTML.
+Declaración de variables, concatenación e impresión de las mismas.
---
+++ Modulo_1_Javascript_Testing/01_sesion/script.js
@@ -0,0 +1,32 @@
+//alert("Hola a todos, bienvenidos");
+
+// Declaración de variables 
+var int = 12345;
+var string = "texto";
+var boolean = true;
+
+var nombre = "Adriana";
+var apellido = "Juarez";
+var nombreCompleto = `${nombre} ${apellido}`
+var edad = 38;
+
+console.log(`Hola, soy ${nombreCompleto} y tengo ${edad} años`);
+console.log("Hola, soy "+ nombreCompleto + " y tengo "+ edad + " años");
+
+
+//Comparación 
+// ==, ===, !=, !==, <, >, >=, <=
+var numero = 7
+var numeroTexto = "8";
+console.log(numero - numeroTexto);
+
+//Repaso
+//Aritmeticos
+//+, -, *, /, %, **
+
+
+//Comparación 
+// ==, ===, !=, !==, <, >, >=, <=
+
+//Logicos
+//&&, ||, !
